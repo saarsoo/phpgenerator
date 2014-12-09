4e38070ceaf8466421c0940bceac3adb03bc8cbf
Johan Saarsoo <johan@saarsoo.se>
2014-12-09 21:41:03 +0100

    Fixed missing interface

--- PHP_Generator_Test/Stubs/BinaryConditionStub.cs
+++ PHP_Generator_Test/Stubs/BinaryConditionStub.cs
@@ -1,8 +1,9 @@
-﻿using PHP_Generator.Structures;
+﻿using PHP_Generator.Generators;
+using PHP_Generator.Structures;
 
 namespace PHP_Generator_Test.Stubs
 {
-    class BinaryConditionStub : GeneratorStub<BinaryCondition>
+    class BinaryConditionStub : GeneratorStub<BinaryCondition>, IBinaryConditionGenerator
     {
     }
 }
